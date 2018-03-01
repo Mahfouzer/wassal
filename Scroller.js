504b7247fdfc1caf1ec35c411f5a80d8f499545d
Nader Mahfouz <mahfouzs96@gmail.com>
2018-03-01 17:39:45 +0200

    Add files via upload
    wrote the core code, still needs improvement to increase usability

---
+++ Scroller.js
@@ -0,0 +1,19 @@
+function wassal(targetElement,addedClass){
+	let windowHeight =  window.innerHeight;
+	let there = targetElement;
+	let tarPosition=0;
+	while( targetElement && !isNaN( targetElement.offsetTop ) ) {
+        tarPosition += targetElement.offsetTop - targetElement.scrollTop;
+        targetElement = targetElement.offsetParent;
+    }
+	window.addEventListener('scroll', function Gotthere(e) {	
+		let currentScrollPositionTop = window.scrollY;
+		let currentScrollPositionBottom = windowHeight + currentScrollPositionTop;
+		if(tarPosition < currentScrollPositionBottom){
+			there.classList.add(addedClass);
+			window.removeEventListener("scroll", Gotthere)
+		}
+	})
+}
+
+// wassal(document.getElementsByClassName("supernova")[2],"lol")
